7a8e207d6160503368f55f8329acaf4586d8a82f
Basmasayed2002 <133095115+Basmasayed2002@users.noreply.github.com>
2023-12-08 13:59:12 +0100

    Add files via upload

---
+++ Database/post.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST Form</title>
+</head>
+<body>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!empty($_POST)) {
+        echo "<h2>Ingevoerde gegevens (POST):</h2>";
+        echo "<p>Naam: " . $_POST['naam'] . "</p>";
+        echo "<p>Achternaam: " . $_POST['achternaam'] . "</p>";
+        echo "<p>Leeftijd: " . $_POST['leeftijd'] . "</p>";
+        echo "<p>Adres: " . $_POST['adres'] . "</p>";
+        echo "<p>Email: " . $_POST['email'] . "</p>";
+    } else {
+        echo "<p>Vul het formulier in om gegevens te verzenden.</p>";
+    }
+}
+?>
+
+<form method="POST" action="post.php">
+    <label for="naam">Naam:</label>
+    <input type="text" name="naam" required><br>
+
+    <label for="achternaam">Achternaam:</label>
+    <input type="text" name="achternaam" required><br>
+
+    <label for="leeftijd">Leeftijd:</label>
+    <input type="number" name="leeftijd" required><br>
+
+    <label for="adres">Adres:</label>
+    <input type="text" name="adres" required><br>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" required><br>
+
+    <input type="submit" value="Verzenden">
+</form>
+
+</body>
+</html>
+
+
+
+
+GET:
+
+Parameters worden toegevoegd aan de URL.
+Gegevens worden zichtbaar in de URL.
+Limiet op de hoeveelheid gegevens die kunnen worden verzonden.
+Kan worden geboekt (gecached) door de browser.
+Minder veilig voor het verzenden van gevoelige informatie, omdat de gegevens zichtbaar zijn in de URL.
+
+POST:
+
+Parameters worden verzonden als een deel van de HTTP-aanvraag, niet zichtbaar in de URL.
+Gegevens zijn niet zichtbaar in de URL en worden niet gecachet door de browser.
+Er is geen beperking aan de hoeveelheid gegevens die kunnen worden verzonden.
+Veiliger voor het verzenden van gevoelige informatie, omdat gegevens niet zichtbaar zijn in de URL.
+Wordt gebruikt wanneer er gegevens moeten worden verzonden die niet in de URL moeten verschijnen (zoals wachtwoorden of grote hoeveelheden gegevens).
